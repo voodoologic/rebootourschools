5baa399bb07d99d768716b6c5d2107ab1b6e4bb1
chris casey <chriscasey1@gmail.com>
2012-02-12 12:57:50 -0800

    init

---
+++ technologytracker/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+
+class District(models.Model):
+    full_name = models.CharField(max_length=60)
+    district_code = models.CharField(max_length=10)
+    state = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    county = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+        return self.district_code
+
+class School(models.Model):
+    district = models.ForeignKey(District)
+    full_name = models.CharField(max_length=60)
+    school_code = models.CharField(max_length=10)
+        
+    def __unicode__(self):
+        return self.full_name 
+        
+    def computer_count(self):
+        return u"%s" % ("num computers")
+    computer_count.short_description = "Computers at this school"           
+    
+
+class Computer(models.Model):
+    school = models.ForeignKey(School)
+    os = models.CharField(max_length=30)
+    processor = models.CharField(max_length=30)
+    hd_size = models.CharField(max_length=10)
+    ram = models.CharField(max_length=30)
+    monitor_size = models.CharField(max_length=30)
+    
+    class Meta:
+        ordering = ('school',)
+        
+    def __unicode__(self):
+        return u"%s %s %s %s" % (self.os, self.processor, self.ram, self.hd_size)
+
+    
+class LicensedSoftware(models.Model):
+    school =  models.ForeignKey(School)
+    product_name = models.CharField(max_length=30)
+    software_type = models.CharField(max_length=30)
+    publisher = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+        return u"%s %s" % (self.product_name, self.publisher)
+
+
+class Printer(models.Model):
+    school =  models.ForeignKey(School)
+    model = models.CharField(max_length=30)
+    manufacturer = models.CharField(max_length=30)
+    kind = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+        return u"%s %s" % (self.manufacturer, self.model)      
+
+
+class Tablet(models.Model):
+    school =  models.ForeignKey(School)
+    model = models.CharField(max_length=30)
+    manufacturer = models.CharField(max_length=30)
+    kind = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+        return u"%s %s" % (self.manufacturer, self.model)
+
+    
+class MobileDevice(models.Model):
+    school =  models.ForeignKey(School)
+    model = models.CharField(max_length=30)
+    manufacturer = models.CharField(max_length=30)
+    kind = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+        return u"%s %s" % (self.manufacturer, self.model)
+    
+    
+    
+    
