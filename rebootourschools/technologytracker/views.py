a965a0da2a6816ea855383de19f2028ab2b45066
Dustin Chesterman <dchester@gmail.com>
2012-02-16 22:58:37 -0800

    Adding start of views
    * Added base template
    * Added sample view that does properly include base template
    * Still need to work out static file management

--- rebootourschools/technologytracker/views.py
+++ rebootourschools/technologytracker/views.py
@@ -1,1 +1,10 @@
+from django.http import HttpResponse
+
+from django.shortcuts import render_to_response
+
+from technologytracker.models import District
 # Create your views here.
+
+def detail(request):
+    #HttpResponse("Blaahhhhh!")
+    return render_to_response('technologytracker/index.html', {})
