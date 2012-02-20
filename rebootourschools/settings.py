7fea83b76f2ca52395e47e39cf8d6741bc326ec5
Dustin Chesterman <dchester@gmail.com>
2012-02-20 00:47:01 -0800

    Adding authentication framework
    Accessing site now requires authentication.  District users can be created through Django admin.

--- rebootourschools/settings.py
+++ rebootourschools/settings.py
@@ -102,7 +102,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-#    'django.contrib.messages.middleware.SessionMiddleware',
 )
 
 ROOT_URLCONF = 'rebootourschools.urls'
--- rebootourschools/technologytracker/views.py
+++ rebootourschools/technologytracker/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render_to_response
 
 from technologytracker.models import District
 # Create your views here.
 
+@login_required(login_url='/techtracker/login/')
 def districtDetail(request):
     #HttpResponse("Blaahhhhh!")
     userDistrict = District.objects.get(pk=1)
     return render_to_response('technologytracker/districtDetail.html', {'userDistrict': userDistrict})
+
